5cbecc75af5425d8d92ac61111a9770ece2cca9c
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-04-16 17:54:41 +0200

    List Pods instead of ProwJobs

--- prow/integration-bootstrap-image/wait.sh
+++ prow/integration-bootstrap-image/wait.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-requiredJobLabelKey=${REQUIRED_JOB_LABEL_KEY:-kyma-component-job}
-requiredJobLabelValue=${REQUIRED_JOB_LABEL_VALUE:-true}
+prowJobId=${PROW_JOB_ID}
+requiredJobLabelKey=${REQUIRED_JOB_LABEL_KEY:-kyma-job-type}
+requiredJobLabelValue=${REQUIRED_JOB_LABEL_VALUE:-component}
 maxRetries=${MAX_RETRIES:-100}
 
-eventGUID=$(kubectl get prowjob ${PROW_JOB_ID} -ojson | jq -r '.metadata."labels"."event-GUID"')
+eventGUID=$(kubectl get pod "${prowJobId}" -ojson | jq -r '.metadata."labels"."event-GUID"')
 
+echo "Prow Job ID: ${prowJobId}"
 echo "GUID: ${eventGUID}"
+echo "Required label: ${requiredJobLabelKey}=${requiredJobLabelValue}"
+echo "Max retries: ${maxRetries}"
 echo "===="
 
 i=0
 until [[ ${i} -eq ${maxRetries} ]]; do
-    notSuccessfullJobs=`kubectl get prowjob -l event-GUID=${eventGUID},${requiredJobLabelKey}=${requiredJobLabelValue} --no-headers -o=custom-columns='NAME:metadata.name,STATE:status.state' | grep -v 'success'`
+    unsuccessfulJobs=$(kubectl get pod -l event-GUID="${eventGUID}","${requiredJobLabelKey}"="${requiredJobLabelValue}" --no-headers -o=custom-columns='NAME:metadata.name,STATE:status.phase' | grep -v 'Succeeded')
 
-    if [[ -z "$notSuccessfullJobs" ]]
+    if [[ -z "$unsuccessfulJobs" ]]
     then
-            echo "No more unsuccessfull component jobs found. Exiting..."
+            echo "No unsuccessful component jobs found. Exiting..."
             exit 0
     fi
 
     # Check if there are any failed jobs. If so, quit early
-    failedJobs=`echo ${notSuccessfullJobs} | grep 'error\|failure' `
+    failedJobs=$(echo "${unsuccessfulJobs}" | grep 'CrashLoopBackOff\|Failed')
 
     if [[ -n "$failedJobs" ]];
     then
-            echo "Jobs with 'failed' state detected. Exiting with code 1..."
+            echo "Jobs with 'Failed' state detected. Exiting with code 1..."
             exit 1
     fi
 
-    printf ">> [${i}/${maxRetries}] Waiting for jobs that should end with success:\n$notSuccessfullJobs\n"
-    
+    echo ">> [${i}/${maxRetries}] Waiting for jobs that should end with success:"
+    printf "%s\n" "${unsuccessfulJobs}"
+
     sleep 5
     ((i++))
 done
